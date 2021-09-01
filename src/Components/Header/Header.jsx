96d4327315f9a101a60aaeb994c2a2a5876cd29e
Aryan <aryan2000sp@gmail.com>
2021-09-02 00:05:04 +0400

    Added responsive header to the page.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,25 @@
+import { ReactComponent as Logo } from "../../images/logo.svg";
+
 const Header = () => {
   return (
     <section className="header-section">
-      <h1>Hey</h1>
+      <article className="header-section-article flex-col flex-col-ai-c">
+        <div className="logo">
+          <Logo />
+        </div>
+        <div className="info flex-col flex-col-ai-c">
+          <h1>
+            Exquisite dining <br /> since 1989
+          </h1>
+          <div className="paragraph">
+            <p className="mobile-paragraph">
+              Experience our seasonal menu in beautiful country surroundings.
+              Eat the freshest produce from the comfort of our farmhouse.
+            </p>
+          </div>
+          <button className="btn-dark">BOOK A TABLE</button>
+        </div>
+      </article>
     </section>
   );
 };
